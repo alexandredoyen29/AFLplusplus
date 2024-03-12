1f135bc91cfb5f4638f0119aded4af8c662e1aa1
Alexandre DOYEN <alexandre.doyen@cea.fr>
2024-03-12 14:48:46 +0100

    Checkpoint

--- custom_mutators/csl_mutator/csl_mutator.c
+++ custom_mutators/csl_mutator/csl_mutator.c
@@ -1,2 +1,58 @@
 #include "csl_mutator.h"
 
+// String lists
+struct stringListNode* stringList_init()
+{
+    return (struct stringListNode*)NULL;
+}
+
+void stringList_addString(struct stringListNode** stringList, char* contentToAdd)
+{
+    struct stringListNode* currentNode = *stringList;
+
+    if (currentNode == (struct stringListNode*)NULL)
+    {
+        currentNode = malloc(sizeof(struct stringListNode));
+
+        assert(currentNode != (struct stringListNode*)NULL);
+
+        currentNode->nodeContent = contentToAdd;
+        currentNode->nextNode = (struct stringListNode*)NULL;
+
+        *stringList = currentNode;
+    }
+    else
+    {
+        while (currentNode->nextNode != (struct stringListNode*)NULL)
+        {
+            currentNode = currentNode->nextNode;
+        }
+
+        currentNode->nextNode = malloc(sizeof(struct stringListNode));
+
+        assert(currentNode->nextNode != (struct stringListNode*)NULL);
+
+        currentNode->nextNode->nodeContent = contentToAdd;
+        currentNode->nextNode->nextNode = (struct stringListNode*)NULL;
+    }
+}
+
+// Fonctions internes pour le mutateur
+void fail()
+{
+    exit(EXIT_FAILURE);
+}
+
+struct cslMutatorIntRep* parseCsl(char* cslContent)
+{
+    struct cslMutatorIntRep* result = malloc(sizeof(struct cslMutatorIntRep));
+
+    assert(result != NULL);
+
+    return result;
+}
+
+// TODO : Sera à retirer à la fin (Ou du moins quand la partie AFL++ sera implémentée)
+int main()
+{
+}
--- custom_mutators/csl_mutator/csl_mutator.h
+++ custom_mutators/csl_mutator/csl_mutator.h
@@ -1,6 +1,29 @@
-#include "afl-fuzz.h"
+//#include "afl-fuzz.h"
 
-void *afl_custom_init(afl_state_t *afl, unsigned int seed);
+#include <stdlib.h>
+#include <assert.h>
+
+struct stringListNode
+{
+    char* nodeContent;
+    struct stringListNode* nextNode;
+};
+
+struct cslMutatorIntRep
+{
+    struct stringListNode* baseInput;
+};
+
+// String lists
+struct stringListNode* stringList_init();
+void stringList_addString(struct stringListNode** stringList, char* contentToAdd);
+
+// Fonctions internes pour le mutateur
+void fail();
+struct cslMutatorIntRep* parseCsl(char* cslContent);
+
+// Fonctions pour AFL++
+/*void *afl_custom_init(afl_state_t *afl, unsigned int seed);
 unsigned int afl_custom_fuzz_count(void *data, const unsigned char *buf, size_t buf_size);
 void afl_custom_splice_optout(void *data);
 size_t afl_custom_fuzz(void *data, unsigned char *buf, size_t buf_size, unsigned char **out_buf, unsigned char *add_buf, size_t add_buf_size, size_t max_size);
@@ -15,4 +38,4 @@ unsigned char afl_custom_queue_get(void *data, const unsigned char *filename);
 void (*afl_custom_fuzz_send)(void *data, const u8 *buf, size_t buf_size);
 u8 afl_custom_queue_new_entry(void *data, const unsigned char *filename_new_queue, const unsigned int *filename_orig_queue);
 const char* afl_custom_introspection(my_mutator_t *data);
-void afl_custom_deinit(void *data);
+void afl_custom_deinit(void *data);*/
