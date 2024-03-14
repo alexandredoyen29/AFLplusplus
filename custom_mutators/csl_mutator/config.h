ae55568c2d97915e48063649ea940bda87852035
Alexandre DOYEN <alexandre.doyen@cea.fr>
2024-03-14 16:25:22 +0100

    Correctif

--- custom_mutators/csl_mutator/config.h
+++ custom_mutators/csl_mutator/config.h
@@ -2,6 +2,6 @@
 #define CONFIG_H
 
 #define MAX_RANDOM_STRING_SIZE  2048
-#define MUTATION_ALPHABET       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+#define MUTATION_ALPHABET       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n"
 
 #endif
