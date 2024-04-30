59fe0b95506d7e01cfbf125d389131edb831c2c1
Alexandre DOYEN <alexandre@soleil>
2024-04-30 15:40:07 +0200

    Enlèvement de code inutile

--- custom_mutators/wildcard_mutator/config.h
+++ custom_mutators/wildcard_mutator/config.h
@@ -1,8 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define MAX_RANDOM_STRING_SIZE          32
-#define MUTATION_ALPHABET               "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 #define MAX_STRING_SIZE                 4096
 #define WILDCARD_FILE_READ_BUFFER_SIZE  2048
 #define ENV_WILDCARD_FILE_PATH          "WILDCARD_MUTATOR_FILE"
